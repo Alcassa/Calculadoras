056126d2736fad4a666abe626c15c563d4ebd61e
Alcassa <galcassa49@gmail.com>
2023-09-25 19:44:56 -0300

    Teste

---
+++ SolucaoCalcs/Calculadoras/FormSuperCalc.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SolucaoCalcs.Calculadoras
+{
+    public partial class FormSuperCalc : Form
+    {
+        public FormSuperCalc()
+        {
+            InitializeComponent();
+        }
+        string vOperacao;
+        double vNumant;
+        bool vLimpar = false;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void f_Numero(object sender, EventArgs e)
+        {
+            string numero = ((Button)sender).ToString();
+            if (vLimpar)
+            {
+                lblPrincipal.Text = "";
+                vLimpar = false;
+            }
+            if (lblPrincipal.Text == "0")
+            {
+                lblPrincipal.Text = "";
+            }
+            lblPrincipal.Text += (sender as Button).Text;
+        }
+        private void operacao(object sender, EventArgs e)
+        {
+            vOperacao = ((Button)sender).Text;
+            vNumant = double.Parse(lblPrincipal.Text);
+            vLimpar = true;
+            lblPrincipal.Focus();
+        }
+
+        private void btnIgual(object sender, EventArgs e)
+        {
+            double vNumdps = double.Parse(lblPrincipal.Text);
+            switch (vOperacao)
+            {
+                case "+":
+                    {
+                        lblPrincipal.Text = (vNumant + vNumdps).ToString();
+                        break;
+                    }
+                case "-":
+                    {
+                        lblPrincipal.Text = (vNumant - vNumdps).ToString();
+                        break;
+                    }
+                case "*":
+                    {
+                        lblPrincipal.Text = (vNumant * vNumdps).ToString();
+                        break;
+                    }
+                case "/":
+                    {
+                        lblPrincipal.Text = (vNumant / vNumdps).ToString();
+                        break;
+                    }
+            }
+
+        }
+
+
+        private void FormSuperCalc_KeyDown(object sender, KeyEventArgs e)
+        {
+            Button but = new Button();
+            if (e.Control == true) { }
+            if (e.Alt == true) { }
+            if (e.Shift == true) { }
+            if ((e.KeyCode >= Keys.D0 && e.KeyCode <= Keys.D9))
+            {
+                but.Text = e.KeyCode.ToString().Substring(1, 1);
+                f_Numero(but, e);
+                return;
+            }
+            else if (e.KeyCode == Keys.Oemcomma)
+            {
+                but.Text = " , ";
+                f_Numero(but, e);
+                return;
+
+            }
+            switch (e.KeyCode.ToString())
+            {
+                case "Oemplus":
+                    {
+                        but.Text = "+";
+                        operacao(but, e);
+                        return;
+                    }
+                case "Subtract":
+                    {
+                        but.Text = "-";
+                        operacao(but, e);
+                        return;
+                    }
+                case "Multiply":
+                    {
+                        but.Text = "*";
+                        operacao(but, e);
+                        return;
+                    }
+                case "'Divide":
+                    {
+                        but.Text = "/";
+                        operacao(but, e);
+                        return;
+                    }
+            }
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnIgual(but, e);
+            }
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            if (lblPrincipal.Text.Length > 1)
+            {
+                lblPrincipal.Text = lblPrincipal.Text.Substring(0, lblPrincipal.Text.Length - 1);
+            }
+            else
+            {
+                lblPrincipal.Text = "0";
+            }
+        }
+    }
+}
---
+++ SolucaoCalcs/login/FormLogin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SolucaoCalcs.MenuPrincipal;
+
+namespace SolucaoCalcs.login
+{
+    public partial class FormLogin_ : Form
+    {
+        public FormLogin_()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtUser.Text.Equals("Alcassa") && txtPassword.Text.Equals("123"))
+                {
+                    var menu = new FormMDI();
+                    menu.Show();
+                    this.Visible = false;
+
+                }
+                else
+                {
+                    MessageBox.Show("Usuario ou senha incorretos");
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Ocorreu um erro");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtUser.Text = "";
+            txtPassword.Text = "";
+        }
+    }
+}
